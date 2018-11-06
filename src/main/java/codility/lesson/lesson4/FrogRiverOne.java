6a5b0f6bf11c7f721b59365a0c5352bf09d6c92c
김현중 <h3045.kim@navercorp.com>
2018-11-06 09:50:59 +0900

    FrogRiverOne solution

---
+++ src/main/java/codility/lesson/lesson4/FrogRiverOne.java
@@ -0,0 +1,35 @@
+package codility.lesson.lesson4;
+
+public class FrogRiverOne {
+
+	private static final int EXITS = 1;
+
+	public static void main(String[] args)
+	{
+		int[] A = {4,1,2,3,1,3,5,1,2};
+		solution(5, A);
+	}
+
+	private static int solution(int X, int[] A) {
+		int[] B = new int[X];
+		int checker = 0;
+
+		for (int i = 0 ; i<A.length; i++){
+
+			if (A[i] > X || B[A[i]-1] == EXITS) {
+				continue;
+			}
+
+			if (B[A[i]-1] != EXITS) {
+				B[A[i]-1] = EXITS;
+				checker++;
+			}
+
+			if (checker == X) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+}
