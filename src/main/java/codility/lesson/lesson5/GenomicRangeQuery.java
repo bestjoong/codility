3076cff6eaf4739f3ce38e00ff10c62808a391d9
h3045.kim <h3045.kim@navercorp.com>
2018-12-11 10:33:46 +0900

    GenomicRangeQuery second commit

--- src/main/java/codility/lesson/lesson5/GenomicRangeQuery.java
+++ src/main/java/codility/lesson/lesson5/GenomicRangeQuery.java
@@ -12,12 +12,48 @@ public class GenomicRangeQuery {
 
 	private static int[] solution(String S, int[] P, int[] Q) {
 		int[] result = new int[P.length];
+		char[] temp = new char[P.length];
 		char[] sa = S.toCharArray();
-		char[][] saa = new char[sa.length][sa.length];
+		//char[][] saa = new char[sa.length][sa.length];
+		char min = 'Z';
+		//char temp;
 
 		for(int i = 0; i < P.length; i++) {
-			for(int j = i; j < P.length; j++) {
-				/*saa[i][j] =*/
+			for(int j = P[i] ; j <= Q[i]; j++) {
+				if (sa[j] == 'A') {
+					min = sa[j];
+					break;
+				} else if (min > sa[j]) {
+					min = sa[j];
+				}
+			}
+			temp[i] = min;
+			min = 'Z';
+		}
+
+
+//		for(int i = 0; i < sa.length; i++) {
+//			min = sa[i];
+//			for (int j = i; j < sa.length; j++) {
+//				if (sa[j] < min) {
+//					min = sa[j];
+//				}
+//				saa[i][j] = min;
+//			}
+//			min = 'Z';
+//		}
+//
+		for(int i = 0; i < P.length; i++) {
+			//temp = saa[P[i]][Q[i]];
+
+			if (temp[i] == 'A') {
+				result[i]=1;
+			} else if (temp[i]=='C') {
+				result[i]=2;
+			} else if (temp[i]=='G') {
+				result[i]=3;
+			} else {
+				result[i]=4;
 			}
 		}
 
